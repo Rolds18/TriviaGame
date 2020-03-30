115577f12697284429d36120e8fb2a5b70ec9a2c
Rolds18 <rolds18@yahoo.com>
2020-03-29 23:24:33 -0500

    added title/start button and filler questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+$("#start").on('click',function(){
+  $('#subwrapper').remove(); 
+})
+
+var questions =[{
+    question:"what was the first full-length CGI movie?",
+    answers:["A Bug's Life","Monster Inc.","Toy Story", "The Lion King"],
+    correctAnswer: "Toy Story",
+
+}, {
+  question:"what was the first full-length CGI movie?",
+  answers:["A Bug's Life","Monster Inc.","Toy Story", "The Lion King"],
+  correctAnswer: "Toy Story",
+
+}, {
+  question:"what was the first full-length CGI movie?",
+  answers:["A Bug's Life","Monster Inc.","Toy Story", "The Lion King"],
+  correctAnswer: "Toy Story",
+
+}, {
+  question:"what was the first full-length CGI movie?",
+  answers:["A Bug's Life","Monster Inc.","Toy Story", "The Lion King"],
+  correctAnswer: "Toy Story",
+
+}, {
+  question:"what was the first full-length CGI movie?",
+  answers:["A Bug's Life","Monster Inc.","Toy Story", "The Lion King"],
+  correctAnswer: "Toy Story",
+
+}, {
+  question:"what was the first full-length CGI movie?",
+  answers:["A Bug's Life","Monster Inc.","Toy Story", "The Lion King"],
+  correctAnswer: "Toy Story",
+
+}, {
+  question:"what was the first full-length CGI movie?",
+  answers:["A Bug's Life","Monster Inc.","Toy Story", "The Lion King"],
+  correctAnswer: "Toy Story",
+
+}, {
+  question:"what was the first full-length CGI movie?",
+  answers:["A Bug's Life","Monster Inc.","Toy Story", "The Lion King"],
+  correctAnswer: "Toy Story",
+
+}]; 
